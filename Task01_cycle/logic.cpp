51b4c5673880dcad9fdcbf590650d473ac2e927a
darya-loboca <dasha.lobova.it@gmail.com>
2026-04-05 08:28:03 +0300

    Task01_cycle solved

--- Task01_cycle/logic.cpp
+++ Task01_cycle/logic.cpp
@@ -1,1 +1,19 @@
 #include "logic.h"
+
+int count_digits(int number) {
+
+	number = number < 0 ? -number : number;
+
+	if (number < 10) {
+		return 1;
+	}
+
+	int count = 0;
+
+	while (number > 0) {
+		number /= 10;
+		count++;
+	}
+
+	return count;
+}
--- Task01_cycle/main.cpp
+++ Task01_cycle/main.cpp
@@ -2,5 +2,14 @@
 
 int main() {
 
+	int number;
+
+	cout << "Input any number: ";
+	cin >> number;
+
+	int result = count_digits(number);
+
+	cout << "Number of digits: " << result;
+
 	return 0;
 }
